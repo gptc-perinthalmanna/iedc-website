6c3db376e44576463c87868ad7966515f2c0e00d
amjed-ali-k <amjedmgm@gmail.com>
2022-12-16 11:02:16 +0530

    fix: svg plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import svgrPlugin from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 // eslint-disable-next-line import/no-unused-modules
 export default defineConfig({
   envDir: './env',
-  plugins: [react(), tsconfigPaths(), svgrPlugin()],
+  plugins: [react(), tsconfigPaths()],
   /* If proxy is needed
   server: {
     proxy: {
