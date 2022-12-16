0b20aeb9668b742161704b3c1cbdf48d5a99e5bd
amjed-ali-k <amjedmgm@gmail.com>
2022-12-16 10:58:12 +0530

    fix: add design and details

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,331 @@
-import { Header } from 'components/Header';
-import { Button } from 'components/Button';
-import { ReactComponent as Logo } from 'assets/favicon.svg';
-
 function App() {
   return (
     <div className="App">
-      <Header title="hola" />
-      <Logo height={100} width={100} />
-      <Button onClick={() => alert('hola')}>Heyo</Button>
+      <div className="bg-neutral-900">
+        <div className="container mx-auto">
+          <section className="mx-auto w-full">
+            <section className="">
+              <div className="mx-auto flex w-full justify-around">
+                <div className="bg-b">
+                  <img
+                    className="h-24 w-14 overflow-hidden rounded-full object-contain py-4"
+                    src="https://www.gptcperinthalmanna.in/_next/image?url=https%3A%2F%2Fi.ibb.co%2FdKm8tsP%2Fbc11ef16fd88.jpg&w=1920&q=75"
+                    alt=""
+                  />
+                </div>
+                <div className="flex items-center">
+                  {/* <a href="" class="text-gray-500 hover:text-gray-50 rounded-sm py-1 duration-200 px-4">ABOUT</a href=""> */}
+                  {/* <a href="" class="text-gray-500 hover:text-gray-50 rounded-sm py-1 duration-200 px-4">ACHIEVEMENTS</a href=""> */}
+                  {/* <a href="" class="text-gray-500 hover:text-gray-50 rounded-sm py-1 duration-200 px-4">TEAM</a href=""> */}
+                  {/* <a href="" class="text-gray-500 hover:text-gray-50 rounded-sm py-1 duration-200 px-4">CONTACT</a href=""> */}
+                </div>
+              </div>
+            </section>
+            <section className="m-8 mx-auto w-full">
+              <div>
+                <div className="tracking text-center text-6xl font-bold tracking-widest text-indigo-600">
+                  IEDC
+                </div>
+                <div className="pt-2 text-center text-xl tracking-[8px] text-gray-500">
+                  GPC PERINTHALMANNA
+                </div>
+                <div />
+              </div>
+            </section>
+            <section>
+              <div className="flex justify-center">
+                <img className="flex w-[720px] justify-center" src="/lightbulb.png" alt="" />
+              </div>
+            </section>
+            <section className="my-28">
+              <div className="text-center text-5xl font-semibold leading-tight text-gray-300">
+                &quot;INNOVATION IS THE ABILITY <br />
+                TO SEE CHANGE AS AN <br />
+                OPPORTUNITY - NOT A THREAT&quot;
+              </div>
+              <div className="text-center text-xl text-gray-500">-Steve Jobs</div>
+            </section>
+            <section>
+              <div className="mx-auto flex max-w-6xl justify-around border-b border-gray-500 pb-28">
+                <div className="text-center">
+                  <div className="flex justify-center">
+                    <img
+                      className="rounded-full bg-blue-800"
+                      src="https://iedc-cet-website.vercel.app/static/media/feature-tile-icon-04.1acd4dacd4ddf306c2e7f5da116b6137.svg"
+                      alt=""
+                    />
+                  </div>
+                  <div>
+                    <div className="text-2xl font-bold tracking-wider text-gray-300">
+                      Innovation
+                    </div>
+                    <div className="py-4 tracking-wide text-gray-500">
+                      We regularly host ideathons and <br />
+                      challenges to help kindle the fire of
+                      <br />
+                      innovation and creativity in budding <br />
+                      entrepreneurs on campus, and bring
+                      <br />
+                      about a culture of idea-driven <br />
+                      development in college.
+                    </div>
+                  </div>
+                </div>
+                <div className="text-center">
+                  <div className="flex justify-center">
+                    <img
+                      className="rounded-full bg-blue-800"
+                      src="https://iedc-cet-website.vercel.app/static/media/feature-tile-icon-05.af2bae113bcfafa4eceac9401c7f3a5a.svg"
+                      alt=""
+                    />
+                  </div>
+                  <div>
+                    <div className="text-2xl font-bold tracking-wider text-gray-300">
+                      Entrepreneurship
+                    </div>
+                    <div className="py-4 tracking-wide text-gray-500">
+                      We provide assistance to startup
+                      <br />
+                      founders in the college, by <br />
+                      collaborating with Kerala Startup
+                      <br />
+                      Mission. We are also <br />
+                      supported by our illustrious alumni <br />
+                      and other leading entrepreneurs and <br />
+                      innovators.
+                    </div>
+                  </div>
+                </div>
+                <div className="text-center">
+                  <div className="flex justify-center">
+                    <img
+                      className="rounded-full bg-blue-800"
+                      src="https://iedc-cet-website.vercel.app/static/media/feature-tile-icon-06.74b0477e38710535bd0e2526d669b6f8.svg"
+                      alt=""
+                    />
+                  </div>
+                  <div>
+                    <div className="text-2xl font-bold tracking-wider text-gray-300">
+                      Technology
+                    </div>
+                    <div className="py-4 tracking-wide text-gray-500">
+                      We regularly conduct workshops,
+                      <br />
+                      competitions, hackathons and panel
+                      <br />
+                      discussions to delve into the latest <br />
+                      technologies and processes that
+                      <br />
+                      drive the industries of today.
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </section>
+            <section>
+              <div className="py-12 text-center text-4xl font-semibold tracking-widest text-gray-300">
+                IEDC at GPC PMNA
+              </div>
+              <div className="mx-auto">
+                <div className="flex justify-evenly">
+                  <div className="flex flex-col">
+                    <div className="text-3xl font-bold text-gray-300">OUR VISION</div>
+                    <div className="w-[480px] py-4 text-xl text-gray-500">
+                      To create an innovation culture among Innovators by introducing them the
+                      State-of-the-art technologies and positioning the Institution as a Learning
+                      and Innovation Platform by delivering technically competent and skilled
+                      Entrepreneurs.
+                    </div>
+                    <div className="w-[420px] py-4 text-xl text-gray-500">
+                      It shall provide assistance to potential entrepreneurs, conduct activities
+                      that develop entrepreneurial qualities in students and shall provide common
+                      facilities to students working on start-ups.
+                    </div>
+                  </div>
+                  <div className="items-center">
+                    <img
+                      className="h-[323px] w-fit justify-end"
+                      src="https://iedc-cet-website.vercel.app/static/media/our_vision.fd212b35d3d826c5fcb8.jpg"
+                      alt=""
+                    />
+                  </div>
+                </div>
+              </div>
+            </section>
+            <section className="mx-auto justify-center py-16">
+              <div className="flex justify-evenly">
+                <div className="flex items-center">
+                  <img className="h-[323px] w-fit justify-end" src="/iedc-1.jpg" alt="" />
+                </div>
+                <div className="flex flex-col">
+                  <div className="text-3xl font-bold text-gray-300">ABOUT IEDC</div>
+                  <div className="w-[420px] py-4 text-xl text-gray-500">
+                    The Innovation and Entrepreneurship Development Cell (IEDC) is an active
+                    student-run cell. Fresh and innovative ideas have started flowing from the
+                    campuses all over India, ever since the governments have started supporting
+                    innovative ventures. The Innovation and Entrepreneurship Development Centre
+                    (IEDC) is being promoted in educational institutions to develop institutional
+                    mechanisms to create entrepreneurial culture in academic institutions and to
+                    foster techno-entrepreneurship for the generation of wealth and employment.
+                  </div>
+                </div>
+              </div>
+            </section>
+            <section className="border-b border-gray-500 pb-28">
+              <div className="flex justify-evenly">
+                <div className="flex flex-col">
+                  <div className="text-3xl font-bold text-gray-300">OBJECTIVES</div>
+                  <div className="w-[480px] py-4 text-xl text-gray-500">
+                    <li>
+                      To motivate the students to think creatively and generate innovative ideas
+                      from all fields of engineering.
+                    </li>
+                    <li>
+                      To transform innovative ideas into socially and industrially relevent
+                      products.&lt;&gt;
+                    </li>
+                    <li>To inculcate a culture of innovation driven entrepreneurship.</li>
+                  </div>
+                </div>
+                <div className="items-center">
+                  <img
+                    className="h-[323px] w-fit justify-end"
+                    src="https://iedc-cet-website.vercel.app/static/media/Objectives.bb2e1904e12600d3fde2.jpg"
+                    alt=""
+                  />
+                </div>
+              </div>
+            </section>
+            <section>
+              <div className="py-12 text-center text-4xl font-semibold tracking-widest text-gray-300">
+                Gallery
+              </div>
+            </section>
+            <section className="border-b border-gray-500 pb-28">
+              <div className="grid grid-cols-3 justify-center gap-3 max-w-5xl mx-auto">
+                <div>
+                  <img className="rounded-2xl h-80 w-full object-cover" src="/event-1.jpg" alt="" />
+                </div>
+                <div>
+                  <img className="rounded-2xl h-80 w-full object-cover" src="/event-2.jpg" alt="" />
+                </div>
+                <div>
+                  <img className="rounded-2xl h-80 w-full object-cover" src="/event-3.jpg" alt="" />
+                </div>
+              </div>
+            </section>
+            <section>
+              <div className="py-12 text-center text-4xl font-semibold tracking-widest text-gray-300">
+                Faculty
+              </div>
+            </section>
+            <section className="pb-28 max-w-5xl mx-auto">
+              <div className="grid grid-cols-3 justify-center gap-6">
+                <div>
+                  <img
+                    className="rounded-2xl h-80 w-full object-cover"
+                    src="/manjusha.jpg"
+                    alt=""
+                  />
+                  <div className="text-center text-gray-500">
+                    <div className="text-xl font-semibold pt-4">Ms. Manjusha</div>
+                    <div className="text-lg pt-1">Principal</div>
+                    <div className="text-lg pt-1">principal@gptcperinthalmanna.in</div>
+                    {/* <div className="text-lg pt-1">+91 1111 222 333</div> */}
+                  </div>
+                </div>
+                {/* <div>
+                  <img
+                    className="rounded-2xl h-80 w-full object-cover"
+                    src="https://iedc-cet-website.vercel.app/static/media/Dr_Gijoy_S.b81f2e94d5e2b255ce8f.png"
+                    alt=""
+                  />
+                  <div className="text-center text-gray-500">
+                    <div className="text-xl font-semibold pt-4">Mr. Amjed Ali K</div>
+                    <div className="text-lg pt-1">Nodal O</div>
+                    <div className="text-lg pt-1">email@gmail.com</div>
+                    <div className="text-lg pt-1">+91 1111 222 333</div>
+                  </div>
+                </div> */}
+                <div>
+                  <img className="rounded-2xl h-80 w-full object-cover" src="/sanjay.jpg" alt="" />
+                  <div className="text-center text-gray-500">
+                    <div className="text-xl font-semibold pt-4">Mr. Sanjay K</div>
+                    <div className="text-lg pt-1">Nodal Officer</div>
+                    <div className="text-lg pt-1">sanjoosurendran@gmail.com</div>
+                    <div className="text-lg pt-1">+91 8943048059</div>
+                  </div>
+                </div>
+              </div>
+            </section>
+            <section>
+              <div className="py-12 text-center text-4xl font-semibold tracking-widest text-gray-300">
+                Team
+              </div>
+            </section>
+            <section className="pb-28 max-w-5xl mx-auto">
+              <div className="grid grid-cols-4 justify-center gap-6">
+                <div>
+                  <img className="rounded-2xl h-80 w-full object-cover" src="/saneen.jpg" alt="" />
+                  <div className="text-center text-gray-500">
+                    <div className="text-xl font-semibold pt-4">Mohammed Saneen</div>
+                    <div className="text-lg pt-1">Student Lead</div>
+                    {/* <div className="text-lg pt-1">principal@gmail.com</div> */}
+                    <div className="text-lg pt-1">+91 98954 85344</div>
+                  </div>
+                </div>
+                {/* <div>
+                  <img
+                    className="rounded-2xl h-80 w-full object-cover"
+                    src="https://iedc-cet-website.vercel.app/static/media/Dr_Gijoy_S.b81f2e94d5e2b255ce8f.png"
+                    alt=""
+                  />
+                  <div className="text-center text-gray-500">
+                    <div className="text-xl font-semibold pt-4">Mr. Amjed Ali K</div>
+                    <div className="text-lg pt-1">Faculty Advisor</div>
+                    <div className="text-lg pt-1">email@gmail.com</div>
+                    <div className="text-lg pt-1">+91 1111 222 333</div>
+                  </div>
+                </div>
+                <div>
+                  <img
+                    className="rounded-2xl h-80 w-full object-cover"
+                    src="https://iedc-cet-website.vercel.app/static/media/Dr_Raju_C.c949309d07c91d11c2b7.png"
+                    alt=""
+                  />
+                  <div className="text-center text-gray-500">
+                    <div className="text-xl font-semibold pt-4">Mr. Sanjay K</div>
+                    <div className="text-lg pt-1">Nodal Officer</div>
+                    <div className="text-lg pt-1">email@gmail.com</div>
+                    <div className="text-lg pt-1">+91 1111 222 333</div>
+                  </div>
+                </div>
+                <div>
+                  <img
+                    className="rounded-2xl h-80 w-full object-cover"
+                    src="https://iedc-cet-website.vercel.app/static/media/Dr_Raju_C.c949309d07c91d11c2b7.png"
+                    alt=""
+                  />
+                  <div className="text-center text-gray-500">
+                    <div className="text-xl font-semibold pt-4">Mr. Sanjay K</div>
+                    <div className="text-lg pt-1">Nodal Officer</div>
+                    <div className="text-lg pt-1">email@gmail.com</div>
+                    <div className="text-lg pt-1">+91 1111 222 333</div>
+                  </div>
+                </div> */}
+              </div>
+            </section>
+            {/* <section> */}
+            {/* <div> */}
+            {/* <div class="">insta</div> */}
+            {/* <div class="">facebook</div> */}
+            {/* <div class="">twitter</div> */}
+            {/* <div class="">linked in</div> */}
+            {/* </div> */}
+            {/* </section> */}
+          </section>
+        </div>
+      </div>
     </div>
   );
 }
